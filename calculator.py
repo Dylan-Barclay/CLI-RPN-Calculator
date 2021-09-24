61a30c200b5273934bacff1fe0f7539e727b6811
Dylan Barclay <dr.barclay99@gmail.com>
2021-09-24 14:14:02 -0500

    Trying to find bugs and fixing them

--- calculator.py
+++ calculator.py
@@ -1,56 +1,62 @@
 class Calculator:
     def calculate(self, input):
-        global stack 
+        global stack
         user_input = input.split(" ")
-        for y in user_input:
-            if y not in ("-", "+", "*", "/","q", "clear", "c", ""):
-                stack.append(int(y))
-            elif y == '+':
+        for input in user_input:
+            if input.lower() not in ("-", "+", "*", "/", "exit", "q", "stop", "close", "clear", "c", ""):
+                stack.append(int(input))
+            elif input == '+' and len(stack)>1:
                 if stack == []:
                     return "Can not add with out an interger"
                 else:
                     val1 = stack.pop()
                     val2 = stack.pop()
                     stack.append(val1 + val2)
-            elif y == '-':
+            elif input == '-' and len(stack)>1:
                 if stack == []:
                     return "Can not subtract with out an intergers"    
                 else:
                     val1 = stack.pop()
                     val2 = stack.pop()
                     stack.append(val2 - val1)
-            elif y == '*':
+            elif input == '*' and len(stack)>1:
                 if stack == []:
                     return "There are no numbers in memory"
                 stack.append(stack.pop() * stack.pop())
-            elif y == '/':
+            elif input == '/' and len(stack)>1:
                 if stack == []:
-                    return "There are no number in memory "
+                    return "There are no number in memory"
                 val1 = stack.pop()
                 val2 = stack.pop()
                 stack.append(val2 / val1)
+            elif input in ("exit", "q", "stop", "close"):
+                print("Thank you for using the calulator... ")
+                print("Now closing the calculator")
+                break
+            elif input == "":
+                print()
+            else:
+                print("I am sorry this Calulator can not preform this action please try another")
+        
+        if len(stack) > 0:
+            print(stack[-1])
+        else:
+            print()
         return stack 
 
 
 import re
 print("Launching the calculator...")
 stack = []
-exit = ("exit", "q", "stop", "close")
 calc = Calculator()
 user_input = ""
-while user_input.lower() not in exit:
+while user_input.lower() not in ("exit", "q", "stop", "close"):
     regex = re.search('[a-zA-Z]', user_input)
     if user_input.lower() in ["c", "clear"]:
         stack = []
         print("The calculator is clearing...")
         print("The calculator is now clear")
-    elif user_input in exit:
-        print("Thank you for using the calulator... ")
-        print("Now closing the calculator")
     elif regex != None:
         print("Please write an expression")
     user_input = input(">")
     output = calc.calculate(user_input)
-    print(stack[-1])
-print("Thank you for using the calulator... ")
-print("Now closing the calculator")
