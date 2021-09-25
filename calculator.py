0ea7f3a4984734303b137ef3d240672e56384c51
Dylan Barclay <dr.barclay99@gmail.com>
2021-09-24 23:26:24 -0500

    Adding helpful comments if you dont have more than one number

--- calculator.py
+++ calculator.py
@@ -1,69 +1,62 @@
 import re
 class Calculator:
-    def calculate(self, input):
-        global stack
-        input = input.split(" ")
+    def calculate(self, user_input):
+        global memory 
+        input = user_input.split(" ")
         for action in input:
-            if action.lower() not in ("-", "+", "*", "/", "exit", "q", "stop", "close", "clear", "c", "", ["a-z"]):
-                stack.append(int(action))
-            elif action == '+' and len(stack)>1:
-                if stack == []:
-                    return  "There are no numbers in memory"
-                elif len(stack) < 1:
-                    return "You need one more number to add please"
-                else:
-                    val1 = stack.pop()
-                    val2 = stack.pop()
-                    stack.append(val1 + val2)
-            elif action == '-' and len(stack)>1:
-                if stack == []:
-                    return  "There are no numbers in memory"    
-                elif len(stack) < 1:
-                    return "You need one more number to add please"
-                else:
-                    val1 = stack.pop()
-                    val2 = stack.pop()
-                    stack.append(val2 - val1)
-            elif action == '*' and len(stack)>1:
-                if stack == []:
-                    return "There are no numbers in memory"
-                elif len(stack) < 1:
-                    return "You need one more number to add please"
-                stack.append(stack.pop() * stack.pop())
-            elif action == '/' and len(stack)>1:
-                if stack == []:
-                    return "There are no number in memory"
-                elif len(stack) < 1:
-                    return "You need one more number to add please"
-                
-                val1 = stack.pop()
-                val2 = stack.pop()
+            if action.lower() not in ("-", "+", "*", "/", "exit", "q", "stop", "close", "clear", "c","cls", ""):
+                memory.append(int(action))
+            elif action in ("-", "+", "*", "/") and len(memory) < 2:
+                print("You need one more number to do this operation...")
+                return
+            elif action == '+' and len(memory)>1:
+                val1 = memory.pop()
+                val2 = memory.pop()
+                memory.append(val1 + val2)
+            elif action == '-' and len(memory)>1:
+                val1 = memory.pop()
+                val2 = memory.pop()
+                memory.append(val2 - val1)
+            elif action == '*' and len(memory)>1:
+                memory.append(memory.pop() * memory.pop())
+            elif action == '/' and len(memory) > 1:
+                val1 = memory.pop()
+                val2 = memory.pop()
                 if val1 == 0:
-                    return(f" You can not divide by zero... please try another number")
-                stack.append(val2 / val1)
+                    print(" You can not divide by zero... please try another number")
+                    return
+                memory.append(val2 / val1)
             elif action in ("exit", "q", "stop", "close"):
                 print("Thank you for using the calulator... ")
                 print("Now closing the calculator")
                 return
             elif action == "":
                 return
+            elif action in ("c", "clear", "cls"):
+                return 
             else:
-                return("I am sorry this Calulator can not preform this action please try another")
+                print("I am sorry this Calulator can not preform this action please try another")
+                print(" ")
+                print("Calculator can only handle + - * / ")
+                return 
         
-        if len(stack) > 0:
-            print(stack[-1])
+        if len(memory) > 0:
+            print(memory[-1])
         else:
             print()
-        return stack 
+        return memory 
 
 
 print("Launching the calculator...")
-stack = []
+print("")
+print("  Clear: c, cls, clear     Exit: q, exit, stop, close")
+print("")
+memory  = []
 calc = Calculator()
 user_input = ""
 while user_input.lower() not in ("exit", "q", "stop", "close"):
     regex = re.search('[a-zA-Z]', user_input)
-    if user_input.lower() in ["c", "clear"]:
+    if user_input.lower() in ["c", "clear", "cls"]:
         stack = []
         print("The calculator is clearing...")
         print("The calculator is now clear")
